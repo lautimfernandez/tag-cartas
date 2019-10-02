2b19a282ca2ae7c6a5de07e1154110d180a58272
roavakian <rocioavakian@gmail.com>
2019-10-01 23:25:23 -0300

    cambio en la vista dashboardcartas

--- src/components/dashboardCarta/index.js
+++ src/components/dashboardCarta/index.js
@@ -8,27 +8,38 @@ import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
 import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
+  import Typography from "@material-ui/core/Typography";
 import { red } from "@material-ui/core/colors";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import Button from '@material-ui/core/Button'
 import { withRouter, Link} from "react-router-dom";
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles(theme => ({
   card: {
-    maxWidth: 345
+    maxWidth: 250
   },
   media: {
     textAlign: 'center',
     justify: 'center',
-    alignContent: 'center',
-    paddingTop: "10%" // 16:9
+    alignContent: 'center'//,
+    //paddingTop: "5%" 
+  },
+  header: {
+    textAlign: 'left', 
+    justify: 'right',
+    backgroundColor: '#E9E9E9'
   },
   problemColor: {
-    backgroundColor: red[400]
+    backgroundColor: red[400],
+    width:'25px',
+    height:'25px'
   },
   withoutProblemColor:{
-    backgroundColor: '#84c456'
+    backgroundColor: '#84c456',
+    width:'25px',
+    height:'25px'
   }
 }));
 
@@ -44,39 +55,31 @@ function DashboardCarta(props) {
   return (
     <Link to={"/pozos/"+pozo+"/cartas/"+c.cardNumber} style={{ textDecoration: 'none' }}>
     <Card className={classes.card}>
-      <CardActionArea >
+      <CardActionArea>
+      <CardContent className={classes.header}>
       <CardHeader
-        className={diagnose==="Sin problemas" ? classes.withoutProblemColor : classes.problemColor}
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        subheader={"Última actualización: "+ fecha}
-      />
-      <CardContent 
-      className={classes.media} >
-      <CardHeader 
-      textAlign= 'center'
-      titleTypographyProps={{ variant:'h2' }}
-      title={"Carta " + c.cardNumber}
+        avatar={
+          <Avatar aria-label="recipe" className={diagnose==="Sin problemas" ? classes.withoutProblemColor : classes.problemColor}>
+          </Avatar>}
+        title={<Typography variant="h5"  component="p" alignContent='right' textAlign='right'>
+          {"Carta " + c.cardNumber}</Typography>}
       />
       </CardContent>
+
       <CardContent 
       className={classes.media} >
         
-        <Typography variant="subtitle1"  component="p">
+        <Typography variant="body2"  component="p">
           {"Diagnóstico: " + diagnose }
         </Typography>
 
-        <Typography variant="subtitle1"  component="p">
-          {"Probabilidad de futuro problema: " + porcentaje }
+        <Typography variant="body2"  component="p">
+          {"Probabilidad: " + porcentaje }
         </Typography>
-        {/*
-        <Typography variant="subtitle1" color='textPrimary' component="p">
-        Última actualización: {JSON.stringify(fecha).slice(9,11)}/{JSON.stringify(fecha).slice(6,8)}/{JSON.stringify(fecha).slice(1,5)}
+
+        <Typography variant="body2"  component="p">
+          {"Última actualización: "+ fecha}
         </Typography>
-        */}
       </CardContent>
         </CardActionArea>
     </Card>
