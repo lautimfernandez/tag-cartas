66d7fd66853bba8cf20487bbcae8f6d0f687a38a
lauti fernandez <lautifernandez@MacBook-Pro-de-lauti.local>
2019-10-02 14:51:02 -0300

    colors added on dashboard

--- src/components/cardsPozos/index.js
+++ src/components/cardsPozos/index.js
@@ -9,7 +9,7 @@ import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-import { red } from "@material-ui/core/colors";
+import { red , yellow, green} from "@material-ui/core/colors";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import Button from '@material-ui/core/Button'
 import { withRouter, Link } from 'react-router-dom';
@@ -28,25 +28,70 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: red[400]
   },
   withoutProblemColor:{
-    backgroundColor: '#84c456'
+    backgroundColor: green[400]
+  },
+  futureProblemColor:{
+    backgroundColor : yellow[400]
   }
 }));
+
+const obtenerDiagnostico = (diagnose) =>{
+  switch(diagnose){
+    case("noProblem"):
+      return "Sin problemas"
+    case(""):
+      return "Sin diagnostico"
+    default:{
+      return diagnose.split(",").map(d => translate(d)).join(" | ");
+    }
+  }
+}
+
+const translate = (diagnose) =>{
+  switch(diagnose){
+    case("gasInterference"):
+      return "Interferncia de gas"
+    case("fluidStroke"):
+      return "Golpe de fluido"
+    case("bombStroke"):
+      return "Golpe de bomba"
+    case("flowingWell"):
+      return "Pozo fluyente"
+    case("fishingRodRods"):
+      return "Pesca de varillas de bombeo"
+  }
+}
+
+const obtenerColor = (diagnose,porcentaje,classes) =>{
+  if(diagnose==="Sin problemas" || diagnose==="Sin diagnostico"){
+    if(porcentaje<20){
+      return classes.withoutProblemColor
+    }
+    else{
+      return classes.futureProblemColor
+    }
+  }
+  else{
+    return classes.problemColor
+  }
+} 
+
 
 function CardsPozos(props) {
   const classes = useStyles();
   const {pozo, carta} = props;
   const c = carta ? carta : {};
-  const diagnose = c.diagnose ? c.diagnose : "Sin problemas";
+  const diagnose = obtenerDiagnostico(c.diagnose);
   const fecha = c.date ? JSON.stringify(c.date).slice(9,11)+"/"+JSON.stringify(c.date).slice(6,8)+"/"+JSON.stringify(c.date).slice(1,5) : "";
-  const porcentaje = c.diagnose ? "80%" : "100%";
-  
+  const porcentaje = diagnose==="Sin problemas"  || diagnose ==="Sin diagnostico" ? Math.random()*100 : 100;
+ 
     
   return (
     <Link to={"/pozos/"+pozo.id} style={{ textDecoration: 'none' }}>
     <Card className={classes.card}>
       <CardActionArea >
       <CardHeader
-        className={diagnose==="Sin problemas" ? classes.withoutProblemColor : classes.problemColor}
+        className={obtenerColor(diagnose,porcentaje,classes)}
         action={
           <IconButton aria-label="settings">
             <MoreVertIcon />
@@ -70,7 +115,7 @@ function CardsPozos(props) {
         </Typography>
 
         <Typography variant="subtitle1"  component="p">
-          {"Probabilidad de futuro problema: " + porcentaje }
+          {"Probabilidad de futuro problema: " + porcentaje.toFixed(2) }
         </Typography>
         {/*
         <Typography variant="subtitle1" color='textPrimary' component="p">
