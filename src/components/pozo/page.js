68e85e3310cb12dc0a7dabb454f79a7898c448c1
rocio <rocioavakian@gmail.com>
2019-10-03 13:21:56 -0300

    diagnostico ult carta pozo

--- src/components/pozo/page.js
+++ src/components/pozo/page.js
@@ -11,7 +11,53 @@ import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
+import { red , yellow, green, grey} from "@material-ui/core/colors";
+import Avatar from '@material-ui/core/Avatar';
 
+const obtenerDiagnostico = (diagnose) =>{
+    switch(diagnose){
+      case("noProblem"):
+        return "Sin problemas"
+      case(""):
+        return "Sin diagnostico"
+      default:{
+        return diagnose.split(",").map(d => translate(d)).join(" | ");
+      }
+    }
+  }
+  
+    
+  const translate = (diagnose) =>{
+    switch(diagnose){
+      case("gasInterference"):
+        return "Interferncia de gas"
+      case("fluidStroke"):
+        return "Golpe de fluido"
+      case("bombStroke"):
+        return "Golpe de bomba"
+      case("flowingWell"):
+        return "Pozo fluyente"
+      case("fishingRodRods"):
+        return "Pesca de varillas de bombeo"
+      default:
+        return ""
+    }
+  }
+    
+  const obtenerColor = (diagnose,porcentaje) =>{
+    if(diagnose==="Sin problemas"){
+      if(porcentaje>60){
+        return "withoutProblemColor"
+      } else {
+        return "futureProblemColor"
+      }
+    } else if(diagnose==="Sin diagnostico") {
+      return "undiagnosedColor"
+    } else {
+      return "problemColor"
+    }
+  }
+  
 
 function Page(props) {
 
@@ -26,8 +72,17 @@ function Page(props) {
     const wellLengths = well ? well.wellLengthInFile.join(" m, ") : "";
     const race = well ? well.race : "";
     const diameters = well ? well.wellDiameters.join(" m, ") : "";
+    const {carta} = props;
+    const c = carta ? carta : {};
+    
+    let diagnose, fecha, porcentaje;
 
-    return Object.keys(wells).length>0 ? (
+    if(Object.keys(pumpCard).length>0) {
+      diagnose = obtenerDiagnostico(cartasPozo[0].diagnose);
+      porcentaje = diagnose==="Sin problemas"  || diagnose ==="Sin diagnostico" ? c.id*100/2173 : 100;
+    }
+    
+    return Object.keys(pumpCard).length>0 ? (
         <Fragment>
             <CssBaseline />
 
@@ -39,11 +94,14 @@ function Page(props) {
                 <Card className="card">
                     <CardActionArea>
                         <CardContent className="header">
-                            <CardHeader
+                            <CardHeader 
+                            avatar={
+                            <Avatar aria-label="recipe" id={obtenerColor(diagnose,porcentaje)}>
+                            </Avatar>}
                                 /*avatar={
                                     <Avatar aria-label="recipe" className={obtenerColor(diagnose,porcentaje,classes)}>
                                     </Avatar>}*/
-                                title={<Typography variant="h5"  component="p" alignContent='right' textAlign='right'>
+                                title={<Typography variant="h5" component="p" alignContent='right' textAlign='right'>
                                 {"Información"}
                                 </Typography>}
                             />
@@ -75,6 +133,15 @@ function Page(props) {
 
                             <Typography variant="body2"  component="p">
                             {"Carrera: "+ race}
+                            </Typography>
+                            
+                            <br></br>
+
+                            <Typography variant="body2"  component="p">
+                            {"Diagnóstico: "+ diagnose}
+                            </Typography>
+                            <Typography variant="body2"  component="p">
+                            {"Porcentaje: "+ porcentaje + "%"}
                             </Typography>
 
                         </CardContent>
